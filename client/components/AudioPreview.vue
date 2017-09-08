94a5c7fe8c764c30eb6f5df3d4bd14eafbc791cd
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-09-08 13:35:50 +0200

    feat(playback): Smooth interactions
    * fading does not work well on mobile safari, deactivate for now
    * make play animation stop when playback completes

--- client/components/AudioPreview.vue
+++ client/components/AudioPreview.vue
@@ -2,56 +2,27 @@
 
 <script>
 import { mapState } from 'vuex'
-const START_VOLUME = 0
-const TARGET_VOLUME = 1
+import { SET_UI_CURRENTLY_PLAYING_TRACK } from '../store/mutation-types'
 
 export default {
   created() {
     this.audioObject = new Audio()
+    this.audioObject.addEventListener('ended', () => this.$store.commit(SET_UI_CURRENTLY_PLAYING_TRACK, null))
     this.interval = null
   },
   computed: {
     ...mapState({ currentTrack: state => state.ui.currentTrack })
   },
   methods: {
-    fadeIn() {
-      this.audioObject.volume = START_VOLUME
-
-      this.interval = setInterval(() => {
-        const newVolume = this.audioObject.volume + 0.1
-
-        if (newVolume < TARGET_VOLUME) {
-          this.audioObject.volume = newVolume
-        } else {
-          this.audioObject.volume = TARGET_VOLUME
-          clearInterval(this.interval)
-        }
-      }, 100)
-    },
-    fadeOut() {
-      this.interval = setInterval(() => {
-        const newVolume = this.audioObject.volume - 0.1
-
-        if (newVolume > START_VOLUME) {
-          this.audioObject.volume = newVolume
-        } else {
-          this.audioObject.volume = START_VOLUME
-          clearInterval(this.interval)
-          this.audioObject.pause()
-        }
-      }, 50)
-    },
     playPreviewAudio() {
       if (this.audioObject.src !== this.currentTrack.previewUrl) {
         this.audioObject.src = this.currentTrack.previewUrl
-        this.fadeIn()
-      } else {
-        this.audioObject.volume = TARGET_VOLUME
+        this.audioObject.volume = 0.9
+        this.audioObject.play()
       }
-      this.audioObject.play()
     },
     stopPreviewAudio() {
-      this.fadeOut()
+      this.audioObject.pause()
     }
   },
   watch: {
