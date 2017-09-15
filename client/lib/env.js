8069681547949daf4785282b2fe756bf24fbae5f
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-09-15 09:07:28 +0200

    feat(tracks): add constant for time update

--- client/lib/env.js
+++ client/lib/env.js
@@ -2,6 +2,7 @@ const isDev = () => process.env.NODE_ENV === 'development'
 
 const isProd = () => process.env.NODE_ENV === 'production'
 
+const ELAPSED_UPDATE_INTERVAL = 60000
 const POLLING_INTERVAL = 45000
 
-export { isDev, isProd, POLLING_INTERVAL }
+export { isDev, isProd, ELAPSED_UPDATE_INTERVAL, POLLING_INTERVAL }
