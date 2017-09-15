0603a87e5608cc732f7162c4d42471dd0276bb68
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-09-15 09:40:15 +0200

    feat(header): Fix icon and positioning

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <svgicon icon="ribbon" width="50" height="50" />
+    <svgicon class="logo" icon="ribbon" width="30" height="50" />
     <svgicon icon="logo" width="110" height="52" />
     <img class="spotify-icon" src="/spotify_icon.png" alt="Spotify Logo" title="powered by" />
   </div>
@@ -18,11 +18,12 @@ export default {
 .container {
   align-items: center;
   background-color: var(--color-dark-bg);
-  color: white;
+  color: var(--color-text);
   display: grid;
-  grid-template-columns: 50px 1fr 40px;
-  padding: var(--grid-cols-1);
-  padding-left: 0;
+  grid-template-columns: calc(4 * var(--grid-column-size)) 5fr 1fr;
+  grid-gap: var(--grid-cols-1);
+  overflow: hidden;
+  padding: var(--grid-cols-2) var(--grid-cols-1);
   position: sticky;
   top: 0;
   z-index: var(--z-index-medium);
@@ -32,8 +33,13 @@ export default {
   }
 }
 
+.logo {
+  justify-self: center;
+}
+
 .spotify-icon {
   height: 40px;
+  justify-self: center;
   width: 40px;
 }
 </style>
